e3b51433a49617a00cea772583561ea1b1d708a3
Div685 <divyeshpatel685@gmail.com>
2021-07-26 20:08:57 +0300

    Add App.js file inside component dir
    - update index.js file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Welcome to CatLife</h1>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
